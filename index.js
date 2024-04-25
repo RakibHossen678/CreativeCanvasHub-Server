2851bf9d06dd85eae88fed87205827ea6af47c07
RakibHossen678 <rh491464@gmail.com>
2024-04-25 21:07:47 +0600

    mongodb and env setup done

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
 const app = express();
-const port = 3000;
+const port = process.env.port || 5000;
 
-//VwgWZY1dgcfTf0gl
-//Assignment10
+// middleware
+app.use(express.json());
+app.use(cors());
 
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.vrdje6l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrdje6l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -39,9 +45,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Wooden craft");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Wooden craft listening on port ${port}`);
 });
