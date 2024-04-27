6ec58219468b51d3036fc5c428b3968e66e28c5c
RakibHossen678 <rh491464@gmail.com>
2024-04-27 13:44:27 +0600

    get single data with email

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const port = process.env.port || 5000;
 
@@ -9,11 +9,7 @@ const port = process.env.port || 5000;
 app.use(express.json());
 app.use(cors());
 
-
-
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrdje6l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrdje6l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,38 +25,43 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const ArtCollection=client.db('Art&CraftDB').collection('crafts')
-
+    const ArtCollection = client.db("Art&CraftDB").collection("crafts");
 
     //read data
-    
-    app.get('/crafts',async(req,res)=>{
-      const cursor=ArtCollection.find()
-      const result=await cursor.toArray()
-      res.send(result)
-    })
 
-    //read single data
+    app.get("/crafts", async (req, res) => {
+      const cursor = ArtCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/crafts/:id',async(req,res)=>{
-      const id=req.params.id
-      const query={_id : new ObjectId(id)}
-      const result=await ArtCollection.findOne(query)
-      res.send(result)
-    })
+    //read single data with id
+
+    app.get("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await ArtCollection.findOne(query);
+      res.send(result);
+    });
+
+    //read single data with email
+
+    app.get("/myCart/:email", async (req, res) => {
+      console.log(req.params);
+      const result = await ArtCollection.find({
+        user_email: req.params.email,
+      }).toArray();
+      res.send(result);
+    });
 
     //create data
 
-    app.post('/crafts',async(req,res)=>{
-      const craft=req.body
-      console.log(craft)
-      const result=await ArtCollection.insertOne(craft)
-      res.send(result)
-    })
-
-
-
-
+    app.post("/crafts", async (req, res) => {
+      const craft = req.body;
+      console.log(craft);
+      const result = await ArtCollection.insertOne(craft);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     console.log(
